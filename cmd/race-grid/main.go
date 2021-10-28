7a21d46962acb9b045372d8db13980c9b8e3a4f6
Aaron Ireland <aarireland@me.com>
2021-10-28 10:48:20 -0400

    Add finish race function/logging

--- cmd/race-grid/main.go
+++ cmd/race-grid/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/aaronireland/go-race/grid"
+	"github.com/aaronireland/go-race/pacing"
 	"github.com/apex/log"
 	"github.com/apex/log/handlers/text"
 )
@@ -34,19 +35,65 @@ func readGridFromFile(filePath string) (grid.CourseGrid, error) {
 	return course, err
 }
 
+func finishRace(race grid.Race, totalDistance pacing.Distance, maxTime pacing.Duration) grid.CourseSegment {
+	var (
+		distanceCompleted pacing.Distance
+		timeElapsed       pacing.Duration
+	)
+
+	for _, segment := range race.Segments {
+		_, distance, duration := segment.Stats()
+		distanceCompleted += distance
+		timeElapsed = timeElapsed.Add(duration)
+		log.Infof("%s time elapsed and %s distance completed so far...", timeElapsed, race.Units.DistanceString(distanceCompleted))
+	}
+
+	timeRemaining := maxTime.Subtract(timeElapsed)
+	distanceRemaining := totalDistance - distanceCompleted
+
+	pace := pacing.Calculate(timeRemaining, distanceRemaining, race.Units)
+	log.Infof("PACE = %s", pace.Rate)
+
+	return grid.CourseSegment{
+		Pace:        pace,
+		Distance:    distanceRemaining,
+		TimeElapsed: timeRemaining,
+		Units:       race.Units,
+	}
+}
+
 func main() {
 	flag.Parse()
 	grid, err := readGridFromFile(*gridFilePath)
 	if err != nil {
 		log.WithError(err).Fatal("Invalid pace grid file")
 	}
-	log.WithField("totalDistance", grid.TotalDistance).WithField("totalDuration", grid.TotalDuration).Infof("Parsed pacing grid with %d segments...", len(grid.Races))
+	courseDistance, err := pacing.ParseDistance(grid.TotalDistance)
+	if err != nil {
+		log.WithError(err).Fatal("Invalid pace grid file")
+	}
 
-	/*
-		var (
-			distanceRemaining float64 = grid.TotalDistance
-			timeRemaining pacing.Duration = grid.TotalTime
+	races := "races"
+	if len(grid.Races) == 1 {
+		races = "race"
+	}
+
+	log.WithField("totalDistance", grid.TotalDistance).WithField("totalDuration", grid.TotalDuration).Infof("Parsed pacing grid with %d %s...", len(grid.Races), races)
+
+	for i, race := range grid.Races {
+		remainingSegment := finishRace(race, courseDistance, grid.TotalDuration)
+		for si, segment := range race.Segments {
+			pace, distance, duration := segment.Stats()
+			d := race.Units.DistanceString(distance)
+			log.WithField("race", i+1).WithField("segment", si+1).Infof("%s completed in %s with a pace of %s", d, duration, pace)
+		}
+		pace, distance, duration := remainingSegment.Stats()
+		d := race.Units.DistanceString(distance)
+		log.WithField("race", i+1).Infof(
+			"A pace of %s is required to complete the remaining %s of %s "+
+				"in %s to achieve a time of %s",
+			pace, d, grid.TotalDistance,
+			duration, grid.TotalDuration,
 		)
-
-	*/
+	}
 }
